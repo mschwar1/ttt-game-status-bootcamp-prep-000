58218e1aa0f48fe1f5e8010f7012de5761d13835
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-28 01:11:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -31,9 +31,7 @@ def won?(board)
         return win_combination
       elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
         return win_combination
-      else
-        return false
       end
+    return false
     end
-  return current_winners
 end
