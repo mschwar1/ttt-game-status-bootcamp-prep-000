e65f7e3b4b7eb88209ae6ea40bcbc6c47a8c4e67
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-28 01:16:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,21 +18,24 @@ WIN_COMBINATIONS = [
 
 def won?(board)
 
-  WIN_COMBINATIONS.each do |win_combination|
-      win_index_1 = win_combination[0] #first position in winning combo
-      win_index_2 = win_combination[1]
-      win_index_3 = win_combination[2]
+  if board.any?{|i| (i == "X" || i =="O")}
 
-      position_1 = board[win_index_1] #what's in that position on board?
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
+    WIN_COMBINATIONS.each do |win_combination|
+        win_index_1 = win_combination[0] #first position in winning combo
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
 
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination
-      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-        return win_combination
-      else
-        false
+        position_1 = board[win_index_1] #what's in that position on board?
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
+
+        if position_1 == "X" && position_2 == "X" && position_3 == "X"
+          return win_combination
+        elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+          return win_combination
+        end
       end
-    end
+  else
+    false
+  end
 end
