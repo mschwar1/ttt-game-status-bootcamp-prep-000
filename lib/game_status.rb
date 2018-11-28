228a98fdb08cf2ae9ce9cbfe0534aa6931165bfe
Mai Schwartz <mai.schwartz@gmail.com>
2018-11-28 01:08:35 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,7 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
+  current_winners = []
   WIN_COMBINATIONS.each do |win_combination|
       win_index_1 = win_combination[0] #first position in winning combo
       win_index_2 = win_combination[1]
@@ -27,9 +28,9 @@ def won?(board)
       position_3 = board[win_index_3]
 
       if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination
+        current_winners >> win_combination
       else
-        return false
+        false
       end
     end
 end
